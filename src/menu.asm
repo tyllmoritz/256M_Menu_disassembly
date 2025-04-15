e781b5dfc26495c854cc9a4a688eb194e2556d51
Robin Bertram <git@robin.bertram.id>
2025-04-15 23:18:59 +0200

    remove comments

--- src/menu.asm
+++ src/menu.asm
@@ -1,4 +1,3 @@
-; Disassembly of "menu.bin"
 ; This file was created with:
 ; mgbdis v2.0 - Game Boy ROM disassembler by Matt Currie and contributors.
 ; https://github.com/mattcurrie/mgbdis
--- src/menu_61/header_checksum.asm
+++ src/menu_61/header_checksum.asm
@@ -6,36 +6,36 @@ SECTION "EntryPoint", ROM0[$150]
 Jump_000_0150:
 
 SECTION "ROM0 - Extra Code", ROM0[$638]
-    inc b                                         ; $0638: $04
-    nop                                           ; $0639: $00
-    nop                                           ; $063a: $00
-    nop                                           ; $063b: $00
-    db $eb                                        ; $063c: $eb
-    jr nz, jr_000_063f                            ; $063d: $20 $00
+    inc b
+    nop
+    nop
+    nop
+    db $eb
+    jr nz, jr_000_063f
 
 jr_000_063f:
-    nop                                           ; $063f: $00
-    sub b                                         ; $0640: $90
-    nop                                           ; $0641: $00
-    ld a, c                                       ; $0642: $79
-    nop                                           ; $0643: $00
-    sub b                                         ; $0644: $90
-    nop                                           ; $0645: $00
-    ld a, c                                       ; $0646: $79
-    nop                                           ; $0647: $00
-    jr c, jr_000_064a                             ; $0648: $38 $00
+    nop
+    sub b
+    nop
+    ld a, c
+    nop
+    sub b
+    nop
+    ld a, c
+    nop
+    jr c, jr_000_064a
 
 jr_000_064a:
-    ld a, c                                       ; $064a: $79
-    nop                                           ; $064b: $00
-    jr c, jr_000_064e                             ; $064c: $38 $00
+    ld a, c
+    nop
+    jr c, jr_000_064e
 
 jr_000_064e:
-    ld a, c                                       ; $064e: $79
-    nop                                           ; $064f: $00
-    nop                                           ; $0650: $00
-    and b                                         ; $0651: $a0
-    ld a, c                                       ; $0652: $79
-    nop                                           ; $0653: $00
-    nop                                           ; $0654: $00
-    ld h, b                                       ; $0655: $60
+    ld a, c
+    nop
+    nop
+    and b
+    ld a, c
+    nop
+    nop
+    ld h, b
